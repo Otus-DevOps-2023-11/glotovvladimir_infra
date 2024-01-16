7f9ea2e523db5a4dce284ade5fad523efd214123
Глотов Владимир <vladimir.v.glotov@gazprombank.ru>
2024-01-16 22:22:26 +0400

    Создан скрипт для создания образа ВМ с установленными ruby и mangodb

--- config-scripts/deploy.sh
+++ config-scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-cd ~/.
-sudo apt install git
+cd ~/. || exit
+sudo apt install -y git
 git clone -b monolith https://github.com/express42/reddit.git
-cd reddit
+cd reddit || exit
 bundle install
 puma -d
 ps aux | grep puma
