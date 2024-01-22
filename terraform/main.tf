fd5ce5b2428046e424b9bc5736bc721300132cb9
Глотов Владимир <vladimir.v.glotov@gazprombank.ru>
2024-01-22 22:40:20 +0400

    Создан скрипт для развертывания ВМ с помощью terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,12 @@
-#terraform {
-#  required_providers {
-#    yandex = {
-#      source  = "yandex-cloud/yandex"
-#      version = ">=0.104"
-#    }
-#  }
-#  required_version = ">=0.13"
-#}
+terraform {
+  required_version = ">=0.13.0"
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = ">=0.94.0"
+    }
+  }
+}
 
 provider "yandex" {
   service_account_key_file = var.service_account_key_file
